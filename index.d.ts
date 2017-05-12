9a47aa6cc49fba25f0965d335bb41d5395b1367d
Christian Tellnes <christian@tellnes.no>
2017-05-12 16:59:32 +0200

    add typescript declaration

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+import Logger = require('bunyan')
+import { Request, Response, RequestHandler } from 'express'
+
+export = middleware
+
+interface Params {
+  headerName?: string
+  propertyName?: string
+  additionalRequestFinishData?: { (req: Request, res: Response): object }
+  logName?: string
+  obscureHeaders?: string[]
+  excludeHeaders?: string[]
+  requestStart?: boolean
+  verbose?: boolean
+  level?: 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal'
+}
+interface ParamsWithLogger extends Params {
+  logger: Logger
+}
+declare function middleware(params: ParamsWithLogger): RequestHandler
+declare function middleware(params: Params, logger: Logger): RequestHandler
+declare function middleware(logger: Logger): RequestHandler
+
+
+declare module 'express' {
+  export interface Request {
+    log: Logger
+  }
+  export interface Response {
+    log: Logger
+  }
+}
